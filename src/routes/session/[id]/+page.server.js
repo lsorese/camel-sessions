3dd42a01062dd475701019b18ac81eefde20d6aa
Logan Sorese <logan.sorese@meetdomaine.com>
2026-01-12 20:56:51 -0500

    Fix session load

--- src/routes/session/[id]/+page.server.js
+++ src/routes/session/[id]/+page.server.js
@@ -1,5 +1,12 @@
-import { getSessionByNumber } from '$lib/sessionData.js';
+import { getSessionByNumber, getAllSessions } from '$lib/sessionData.js';
 import { error } from '@sveltejs/kit';
+
+export function entries() {
+  const sessions = getAllSessions();
+  return sessions.map(session => ({
+    id: session.session.toString()
+  }));
+}
 
 export function load({ params }) {
   const session = getSessionByNumber(params.id);
